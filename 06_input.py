54761d3a6b4bd102c4d3e53356755160d50700f8
Amrit Ranjan <2amritranjan@gmail.com>
2025-01-31 11:12:00 +0000

    input

---
+++ 06_input.py
@@ -0,0 +1,8 @@
+input("What is your name?")
+
+#Here input function is used to take input from user.
+
+name = input("What is your name?")
+print(f'Hey {name}, have a great day.')
+
+# We are using name as a variable to print a greeting message.
