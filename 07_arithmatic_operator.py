eca04b19b2b9f5e938a072bc9c777154c23d8143
Amrit Ranjan <2amritranjan@gmail.com>
2025-01-31 11:21:31 +0000

    push

---
+++ 07_arithmatic_operator.py
@@ -0,0 +1,9 @@
+x = 12
+y = 21
+a = 10
+b = 8
+
+print(x + y)     # To add numbers we use + sign
+print (x - b)    #To subtract numbers we use - sign
+print (y * a)    #For multiplication we use  * sign
+prnit(y / a)     #For division we use / sign
