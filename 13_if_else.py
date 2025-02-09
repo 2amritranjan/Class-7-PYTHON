7761b7accf8e3dd95dd81f438ce91f339a5d9083
Amrit Ranjan <2amritranjan@gmail.com>
2025-02-09 13:43:08 +0000

    if else and for loop added.

--- 13_if_else.py
+++ 13_if_else.py
@@ -17,3 +17,15 @@ if userName == "Raju":
     print("Amrit is online.")    #Here userName is not "Amrit" as we are checking for "Raju", so this condition will not be printed.
 else:
     print("Wrong user.")         #Here we are checkin for Raju so this condition will be printed.
+
+# Here we are again tring to learn something about if else conditioning....
+
+if (12 == "12"):
+    print("This is equal.")
+else:
+    print("Not equal.")   #As we already know that 12 is not equal to "12" as one is int and other is str in datatype.
+
+if (24 < 25):
+    print("24 is smaller than 25.")
+else:
+    print("24 is greater")   #Here i hope you already know the result now.
---
+++ 14_for_loop.py
@@ -0,0 +1,17 @@
+"""
+Now we are going to learn about for loop.
+
+"""
+
+someValues = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+for numbers in someValues:
+    print(numbers)
+
+#Here we print each and every values of someValues individually.
+"""
+Here for loop saved alot of our efforts by only writing very few line of code to print all numbers individually.
+
+"""
+
+for value in range (1, 101):
+    print(value)      #Here we printed 1 to 100 within few seconds with help of for loop...
